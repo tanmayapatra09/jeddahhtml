f4f8a91cd2b71a4adf7942563ce99b0d5e0d8374
amtechit <68071192+amtechit@users.noreply.github.com>
2021-07-18 13:42:19 +0530

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,4 @@
 
-
 $(document).on("input paste focus", "#input_search,#input_search_mobile", function() {
     $("#response_search_results, #response_search_results_mobile").show()
 });
@@ -9,6 +8,20 @@ $(document).on("click", function(a) {
         $("#response_search_results, #response_search_results_mobile").hide()
     }
 });
+
+
+$(document).on("input paste focus", "#input_location", function() {
+    $("#response_search_location").show()
+});
+
+$(document).on("click", function(a) {
+    if ($(a.target).closest("#input_location").length === 0) {
+        $("#response_search_location").hide()
+    }
+});
+
+
+
 
 
 $('.wishlist').on('click', function(){
